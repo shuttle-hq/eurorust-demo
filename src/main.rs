1d829a59289e8dcf19e41fbc48bab19a937b811b
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-10-02 13:23:45 +0300

    Add doc comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+#![allow(unused_imports)]
+
 pub mod todo;
 
+use anyhow::anyhow;
 use axum::{
     routing::{get, post},
     Extension, Router,
@@ -9,27 +12,54 @@ use sqlx::PgPool;
 use std::{path::PathBuf, sync::Arc};
 use tower_http::services::ServeDir;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+async fn hello_shuttle() -> &'static str {
+    "Hello from Shuttle!"
 }
 
+// Define a Shuttle runtime main function for serving a static folder.
 #[shuttle_runtime::main]
-async fn axum(
-    #[shuttle_shared_db::Postgres] pool: PgPool,
-    #[shuttle_secrets::Secrets] secret_store: shuttle_secrets::SecretStore,
+async fn static_folder(
     #[shuttle_static_folder::StaticFolder(folder = "assets")] static_folder: PathBuf,
 ) -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+        .route("/", get(hello_shuttle))
+        .nest_service("/assets", ServeDir::new(static_folder));
+
+    Ok(router.into())
+}
+
+// Define a Shuttle runtime main function for handling secrets.
+#[shuttle_runtime::main]
+async fn secrets(
+    #[shuttle_secrets::Secrets] secret_store: shuttle_secrets::SecretStore,
+) -> shuttle_axum::ShuttleAxum {
+    // Retrieve the 'API_KEY' secret from the secret store.
+    let secret = if let Some(secret) = secret_store.get("API_KEY") {
+        secret
+    } else {
+        return Err(anyhow!("secret was not found").into());
+    };
+
+    let router = Router::new().route("/", get(hello_shuttle));
+
+    Ok(router.into())
+}
+
+// Define a Shuttle runtime main function for handling a shared PostgreSQL database.
+#[shuttle_runtime::main]
+async fn shared_db(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
+    // Run database migrations using SQLx.
     sqlx::migrate!()
         .run(&pool)
         .await
         .map_err(CustomError::new)?;
 
+    // Create an Axum router with multiple routes and add a PostgreSQL database connection pool as an extension.
     let router = Router::new()
-        .route("/", get(hello_world))
+        .route("/", get(hello_shuttle))
         .route("/todo", post(todo::add))
         .route("/todo/:id", get(todo::retrieve))
-        .layer(Extension(Arc::new(pool)))
-        .nest_service("/assets", ServeDir::new(static_folder));
+        .layer(Extension(Arc::new(pool)));
 
     Ok(router.into())
 }
