cf583c766371a42970aeaf6b63e460b6e69e5037
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-10-02 13:39:42 +0300

    Move handler to main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,57 @@
 #![allow(unused_imports)]
 
-pub mod todo;
-
 use anyhow::anyhow;
 use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
     routing::{get, post},
-    Router,
+    Json, Router,
 };
+use serde::{Deserialize, Serialize};
 use shuttle_runtime::CustomError;
-use sqlx::PgPool;
+use sqlx::{FromRow, PgPool};
 use std::path::PathBuf;
 use tower_http::services::ServeDir;
+
+#[derive(Deserialize)]
+pub struct TodoNew {
+    pub note: String,
+}
+
+#[derive(Serialize, FromRow)]
+pub struct Todo {
+    pub id: i32,
+    pub note: String,
+}
+
+pub async fn retrieve_todo(
+    Path(id): Path<i32>,
+    State(pool): State<PgPool>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    match sqlx::query_as::<_, Todo>("SELECT * FROM todos WHERE id = $1")
+        .bind(id)
+        .fetch_one(&pool)
+        .await
+    {
+        Ok(todo) => Ok((StatusCode::OK, Json(todo))),
+        Err(e) => Err((StatusCode::BAD_REQUEST, e.to_string())),
+    }
+}
+
+pub async fn add_todo(
+    State(pool): State<PgPool>,
+    Json(data): Json<TodoNew>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    match sqlx::query_as::<_, Todo>("INSERT INTO todos (note) VALUES ($1) RETURNING id, note")
+        .bind(&data.note)
+        .fetch_one(&pool)
+        .await
+    {
+        Ok(todo) => Ok((StatusCode::CREATED, Json(todo))),
+        Err(e) => Err((StatusCode::BAD_REQUEST, e.to_string())),
+    }
+}
 
 async fn hello_shuttle() -> &'static str {
     "Hello from Shuttle!"
@@ -55,8 +96,8 @@ async fn shared_db(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum:
     // Create an Axum router with multiple routes and add a PostgreSQL database connection pool as an extension.
     let router = Router::new()
         .route("/", get(hello_shuttle))
-        .route("/todo", post(todo::add))
-        .route("/todo/:id", get(todo::retrieve))
+        .route("/todo", post(add_todo))
+        .route("/todo/:id", get(retrieve_todo))
         .with_state(pool);
 
     Ok(router.into())
