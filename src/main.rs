336ab1765e566526c25f6a5710fff35fe3409159
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-10-05 11:23:28 +0300

    refactor: use a single endpoint

--- src/main.rs
+++ src/main.rs
@@ -57,36 +57,8 @@ async fn hello_shuttle() -> &'static str {
     "Hello from Shuttle!"
 }
 
-// Define a Shuttle runtime main function for serving a static folder.
 #[shuttle_runtime::main]
-async fn static_folder() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new()
-        .route("/", get(hello_shuttle))
-        .nest_service("/assets", ServeDir::new(PathBuf::from("assets")));
-
-    Ok(router.into())
-}
-
-// Define a Shuttle runtime main function for handling secrets.
-#[shuttle_runtime::main]
-async fn secrets(
-    #[shuttle_secrets::Secrets] secret_store: shuttle_secrets::SecretStore,
-) -> shuttle_axum::ShuttleAxum {
-    // Retrieve the 'API_KEY' secret from the secret store.
-    let secret = if let Some(secret) = secret_store.get("API_KEY") {
-        secret
-    } else {
-        return Err(anyhow!("secret was not found").into());
-    };
-
-    let router = Router::new().route("/", get(hello_shuttle));
-
-    Ok(router.into())
-}
-
-// Define a Shuttle runtime main function for handling a shared PostgreSQL database.
-#[shuttle_runtime::main]
-async fn shared_db(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
+async fn shuttle_main(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
     // Run database migrations using SQLx.
     sqlx::migrate!()
         .run(&pool)
@@ -98,7 +70,8 @@ async fn shared_db(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum:
         .route("/", get(hello_shuttle))
         .route("/todo", post(add_todo))
         .route("/todo/:id", get(retrieve_todo))
-        .with_state(pool);
+        .with_state(pool)
+        .nest_service("/assets", ServeDir::new(PathBuf::from("assets")));
 
     Ok(router.into())
 }
